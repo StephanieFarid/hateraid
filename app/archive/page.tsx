1b11e3e77486af85b615332f5ff68ea66354d737
hanie <stephaniefarid@gmail.com>
2026-09-08 12:43:17 -0400

    adding to archive and 'back to today' button

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const page = () => {
   return (
@@ -8,8 +9,13 @@ const page = () => {
             <br />
             <br />
             <p>
-                {/* people who like music */}
+                toes out in public, its unnccesary and very gross.
             </p>
+            <button className="font-mono fixed bottom-6 right-6 z-50 bg-pink-100 text-black px-4 py-2 rounded-full shadow-lg hover:bg-pink-200 transition" >
+            <Link href={`/`}>
+                back to today
+            </Link>
+        </button>
         </h1>
     </div>
   )
