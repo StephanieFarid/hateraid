be96e590cb5b8544e19e811f84a300a07b05ddc8
hanie <stephaniefarid@gmail.com>
2026-09-08 12:39:40 -0400

    adding archive button

---
+++ app/archive/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className="font-mono">
+            previously I hated...
+            <br />
+            <br />
+            <p>
+                {/* people who like music */}
+            </p>
+        </h1>
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import LiveDate from '@/components/Livedate'
+import Link from 'next/link'
+import LiveDate from '@/app/components/Livedate'
 
 const page = () => {
   return (
@@ -10,6 +11,11 @@ const page = () => {
       <br />
       <br />
       Today I'm hating on people who are so into music that it makes an actual difference to them. They're weird and I don't like it. It's not that serious. 
+            <button className="font-mono fixed bottom-6 right-6 z-50 bg-pink-100 text-black px-4 py-2 rounded-full shadow-lg hover:bg-pink-200 transition" >
+            <Link href={`/archive`}>
+                archive
+            </Link>
+        </button>
     </div>
   )
 }
