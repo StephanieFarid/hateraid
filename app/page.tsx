941f4a98a98b85e40e3939e62e5ad179c3dda5cc
hanie <stephaniefarid@gmail.com>
2026-09-08 21:18:57 -0400

    corrected todays post

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
       Dear diary,
       <br />
       <br />
-      Today I'm hating on people who are so into music that it makes an actual difference to them. They're weird and I don't like it. It's not that serious. 
+      Today I'm hating on people who are so into music that they make it a big deal and like it's part of their personality. Some said to me last week "music is my life it runs through my veins" and I threw up a little in my mouth. They're weird and I don't like it. It's not that serious. 
             <button className="font-mono fixed bottom-6 right-6 z-50 bg-pink-100 text-black px-4 py-2 rounded-full shadow-lg hover:bg-pink-200 transition" >
             <Link href={`/archive`}>
                 archive
