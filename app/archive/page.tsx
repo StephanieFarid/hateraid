745cb46a83c43e017c2e1ce83e4eed777533ecce
hanie <stephaniefarid@gmail.com>
2026-09-10 13:36:30 -0400

    new entry

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -4,13 +4,26 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div>
-        <h1 className="font-mono">
+        <h1 className="font-mono p-20">
             previously I hated...
             <br />
             <br />
             <p>
                 toes out in public, its unnccesary and very gross.
+                 <br />
+                      Inspired by: The guy on my plane ride back from Guatemala wearing flip flops
+
             </p>
+            <br />
+            <br />
+            <p>
+                people who make music their entire personality.
+                      <br />
+                      Inspired by: Someone who said to me "music is my life it runs through my veins".
+
+            </p>
+
+
             <button className="font-mono fixed bottom-6 right-6 z-50 bg-pink-100 text-black px-4 py-2 rounded-full shadow-lg hover:bg-pink-200 transition" >
             <Link href={`/`}>
                 back to today
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import LiveDate from '@/app/components/Livedate'
 
 const page = () => {
   return (
-    <div className="font-mono">
+    <div className="font-mono p-20">
       <LiveDate />
       <br />
       Dear diary,
       <br />
       <br />
-      Today I'm hating on people who are so into music that they make it a big deal and like it's part of their personality. Someone said to me last week "music is my life it runs through my veins" and I threw up a little in my mouth. They're weird and I don't like it. It's not that serious. 
+      Today I'm hating on old people that play in their mouth in pblic. I was at the airport and an OLD WOMAN was chatting with a few others
+      and straight up flossing her teeth with a floss pick. ewwwWWWWWW.
             <button className="font-mono fixed bottom-6 right-6 z-50 bg-pink-100 text-black px-4 py-2 rounded-full shadow-lg hover:bg-pink-200 transition" >
             <Link href={`/archive`}>
                 archive
